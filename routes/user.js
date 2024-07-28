3718a9d5c74899127cbfdb6686935b933d2a6602
AdarshSahu <100473287+adarshsahu460@users.noreply.github.com>
2024-07-28 15:27:45 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,7 @@ const prisma = PrismaSingleton.getInstance()
 const app = express.Router()
 app.use(cookieParser())
 app.use(cors({
-    origin:['https://d1j1rq5bsho0t3.cloudfront.net',`http://152.59.13.210`]
+    origin:['https://d1j1rq5bsho0t3.cloudfront.net',`http://152.59.13.210`,'http://172.20.10.2']
     credentials:true
 }));
 
